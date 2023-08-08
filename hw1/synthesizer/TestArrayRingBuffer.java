9ffb8e64f15b3d85fd384445ce6c3919c87a24e5
FOOLSIX <2580368016@qq.com>
2023-08-08 11:07:57 +0800

    hw1 1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,4 +1,5 @@
 package synthesizer;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,9 +10,40 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(4);
+        arb.enqueue(1);
+        arb.enqueue(10);
+        arb.enqueue(100);
+        arb.enqueue(1000);
+        assertTrue(arb.isFull());
+        assertEquals(1, (long) arb.dequeue());
+        assertEquals(10, (long) arb.dequeue());
+        assertEquals(100, (long) arb.dequeue());
+        assertEquals(1000, (long) arb.dequeue());
+        assertTrue(arb.isEmpty());
+        arb.enqueue(1);
+        arb.enqueue(10);
+        arb.enqueue(100);
+        arb.enqueue(1000);
+        assertEquals(1, (long) arb.dequeue());
+        assertEquals(10, (long) arb.dequeue());
+        assertEquals(100, (long) arb.dequeue());
+        assertEquals(1000, (long) arb.dequeue());
     }
+    @Test
+    public void iteratorTest() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(4);
+        arb.enqueue(1);
+        arb.enqueue(10);
+        arb.enqueue(100);
+        arb.enqueue(1000);
+        int idx = 0;
+        for (Integer n : arb) {
+            System.out.println(n);
+        }
 
+        System.out.println("IteratorTest!");
+    }
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
