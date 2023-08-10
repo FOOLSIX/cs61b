8417008a9932ca50327addd1c4cf9721d7a54c76
FOOLSIX <2580368016@qq.com>
2023-08-10 12:42:25 +0800

    p2j 1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -2,6 +2,9 @@ package byog.Core;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+import java.util.Random;
 
 public class Game {
     TERenderer ter = new TERenderer();
@@ -9,6 +12,86 @@ public class Game {
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
 
+    private TETile[][] t;
+    private enum DIRECTION {
+        UP, RIGHT, DOWN, LEFT;
+
+    }
+    Game() {
+        ter.initialize(WIDTH, HEIGHT);
+        t = new TETile[WIDTH][HEIGHT];
+        //Init TETile
+        for (int i = 0; i < WIDTH; ++i) {
+            for (int j = 0; j < HEIGHT; ++j) {
+                t[i][j] = Tileset.NOTHING;
+            }
+        }
+    }
+    private final int[][] nxt = {{0, 1}, {1, 0}, {0, -1}, {-1, 0},
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+    private void exitGame() {
+        //TODO: save
+
+        System.exit(0);
+    }
+    private void loadGame() {
+        //TODO
+    }
+
+    private long strToSeed(String s) {
+        int idx = 1;
+        long ret = 0L;
+        while(s.charAt(idx) != 'S' && s.charAt(idx) != 's') {
+            ret = ret * 10 +(s.charAt(idx) - '0');
+            idx++;
+        }
+        return ret;
+    }
+
+    private boolean inValid(int x, int y) {
+        return (x > 0 && y > 0 && x < WIDTH - 1 && y < HEIGHT - 1);
+    }
+
+    private void drawWall() {
+        for (int i = 1; i < WIDTH - 1; ++i) {
+            for (int j = 1; j < HEIGHT - 1; ++j) {
+                if (t[i][j] == Tileset.FLOOR) {
+                    for (int k = 0; k < 8; ++k) {
+                        int nx = i + nxt[k][0], ny = j + nxt[k][1];
+                        if(t[nx][ny] == Tileset.NOTHING) {
+                            t[nx][ny] = Tileset.WALL;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private void drawLine(int x, int y, int len, DIRECTION d) {
+        int dir = d.ordinal();
+        while (len-- >= 0) {
+            if(inValid(x, y)) {
+                t[x][y] = Tileset.FLOOR;
+            } else {
+                break;
+            }
+            x += nxt[dir][0];
+            y += nxt[dir][1];
+        }
+    }
+
+    private void generateTile(long seed) {
+        Random rand = new Random(seed);
+        //a tes
+        for (int i = 0; i < 20; ++i) {
+            int x = RandomUtils.uniform(rand, 1, WIDTH - 1);
+            int y = RandomUtils.uniform(rand, 1, HEIGHT - 1);
+            int len = RandomUtils.uniform(rand, 3, 15);
+            DIRECTION d = DIRECTION.values()[RandomUtils.uniform(rand, 0, 4)];;
+            drawLine(x, y, len, d);
+        }
+        drawWall();
+        return;
+    }
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
      */
@@ -31,8 +114,25 @@ public class Game {
         // TODO: Fill out this method to run the game using the input passed in,
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
+        long seed = 0L;
+        switch (input.charAt(0)) {
+            case 'N':
+            case 'n':
+                seed = strToSeed(input);
+                break;
+            case 'Q':
+            case 'q':
+                exitGame();
+            case 'L':
+            case 'l':
+                loadGame();
+            default:
+                break;
+        }
 
-        TETile[][] finalWorldFrame = null;
-        return finalWorldFrame;
+        generateTile(seed);
+        //tes
+        //ter.renderFrame(t);
+        return t;
     }
 }
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,34 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 15;
+    private static final int HEIGHT = 15;
+
+    public static void addHexagon(TETile[][] g, int x, int y, int sz, TETile t) {
+        for(int i = 0; i < sz;++i) {
+            int startx = sz - 1 - i;
+            for(int j = 0; j < sz + 2 * i; ++j) {
+                g[x + startx + j][y + i] = t;
+                g[x + startx + j][y + 2 * sz - 1 - i] = t;
+            }
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile grid[][] = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                grid[x][y] = Tileset.NOTHING;
+            }
+        }
+        addHexagon(grid, 0, 0, 2, Tileset.FLOWER);
+        addHexagon(grid, 3, 2, 2, Tileset.WALL);
+        addHexagon(grid, 6, 0, 2, Tileset.FLOWER);
+
+        ter.renderFrame(grid);
+    }
 }
