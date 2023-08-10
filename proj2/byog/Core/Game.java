5bd92e218e2d993d7b1b677e599eb440b4e24fa2
FOOLSIX <2580368016@qq.com>
2023-08-10 12:48:07 +0800

    p2j 1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -17,8 +17,8 @@ public class Game {
         UP, RIGHT, DOWN, LEFT;
 
     }
-    Game() {
-        ter.initialize(WIDTH, HEIGHT);
+    public Game() {
+        //ter.initialize(WIDTH, HEIGHT);
         t = new TETile[WIDTH][HEIGHT];
         //Init TETile
         for (int i = 0; i < WIDTH; ++i) {
