2dd5a04a49ae0f4cbe80611fb51dbe3b5cac2bc7
FOOLSIX <2580368016@qq.com>
2023-08-15 22:14:44 +0800

    hw2 1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,76 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private final WeightedQuickUnionUF PERCOLATION_UNION;
+    private final int VIRTUAL_TOP;
+    private final int VIRTUAL_BOTTOM;
+    private final int[][] NEAR = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int numOfOpenSites = 0;
+    private boolean[][] g;
+    private final int N;
+    /** create N-by-N grid, with all sites initially blocked */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
 
+        PERCOLATION_UNION = new WeightedQuickUnionUF(N * N + 2);
+        VIRTUAL_TOP = N * N;
+        VIRTUAL_BOTTOM = N * N + 1;
+        g = new boolean[N][N];
+        this.N = N;
+        for (int i = 0; i < N; ++i) {
+            PERCOLATION_UNION.union(VIRTUAL_TOP, i);
+            PERCOLATION_UNION.union(VIRTUAL_BOTTOM, i + N * (N - 1));
+        }
+    }
+    private boolean validPos(int row, int col) {
+        return (row >= 0 && col >= 0 && row < N && col < N);
+    }
+
+    /** open the site (row, col) if it is not open already */
+    public void open(int row, int col) {
+        if (!validPos(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (g[row][col]) {
+            return;
+        }
+
+        g[row][col] = true;
+        ++numOfOpenSites;
+        for (int i = 0; i < 4; ++i) {
+            int nx = row + NEAR[i][0];
+            int ny = col + NEAR[i][1];
+            if (validPos(nx, ny) && g[nx][ny]) {
+                PERCOLATION_UNION.union(nx * N + ny, row * N + col);
+            }
+        }
+    }
+    public boolean isOpen(int row, int col) {
+        if (!validPos(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return g[row][col];
+    }
+    public boolean isFull(int row, int col) {
+        if (!validPos(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return (g[row][col] && PERCOLATION_UNION.connected(VIRTUAL_TOP, row * N + col));
+    }
+
+    /** number of open sites
+     */
+    public int numberOfOpenSites() {
+        return numOfOpenSites;
+    }
+
+    /** does the system percolate?
+     */
+    public boolean percolates() {
+        return PERCOLATION_UNION.connected(VIRTUAL_BOTTOM, VIRTUAL_TOP);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,75 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
 
+public class PercolationStats {
+    private Percolation tes;
+    private int T;
+    private  int N;
+    PercolationFactory pf;
+    private double u;
+    private double chiSquared;
+    double[] x;
+    /** perform T independent experiments on an N-by-N grid
+     *
+     */
+    private void startTest() {
+        double sum = 0;
+        for (int i = 0; i < T; ++i) {
+            int times = 0;
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int ax = StdRandom.uniform(N);
+                int ay = StdRandom.uniform(N);
+                if (!p.isOpen(ax, ay)) {
+                    ++times;
+                    p.open(ax, ay);
+                }
+            }
+            double xi = (double) times / (N * N);
+            sum += xi;
+            x[i] = xi;
+        }
+        u = sum / T;
+    }
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+        x = new double[T];
+        startTest();
+        double s = 0;
+        for (int i = 0; i < T; ++i) {
+            s += (x[i] - u) * (x[i] - u);
+        }
+        chiSquared = s / (T - 1);
+    }
+
+    /** sample mean of percolation threshold
+     *
+     */
+    public double mean() {
+        return u;
+    }
+
+    /** sample standard deviation of percolation threshold
+     *
+     */
+    public double stddev() {
+        if (T == 1) {
+            return Double.NaN;
+        }
+        return chiSquared;
+    }
+
+    /** low endpoint of 95% confidence interval
+     *
+     */
+    public double confidenceLow()  {
+        return (u - 1.96 * Math.sqrt(chiSquared) / Math.sqrt(T));
+    }
+
+    public double confidenceHigh() {
+        return (u + 1.96 * Math.sqrt(chiSquared) / Math.sqrt(T));
+    }
 }
